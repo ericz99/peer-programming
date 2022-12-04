94a7739a823a1fbb1863c5f238c7eb9ec7411d33
ericz99 <ericzhang128@gmail.com>
2022-12-04 11:11:04 -0500

    added .sequelizerc

---
+++ server/src/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: 3306,
+    dialect: 'mysql'
+  },
+  test: {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: 3306,
+    dialect: 'mysql'
+  },
+  production: {
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: 3306,
+    dialect: 'mysql'
+  }
+};
+
+console.log(config);
+
+module.exports = config;
